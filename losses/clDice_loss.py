94c7baa9d8d8b4574d6bda01d98a144e4e6e2c23
valhassan <victor.alhassan@canada.ca>
2021-03-09 09:03:26 -0500

    commented CL code to use CE

--- losses/clDice_loss.py
+++ losses/clDice_loss.py
@@ -64,50 +64,50 @@ class clDiceLoss(nn.Module):
         # print('output_shape:', output.shape, 'target_shape:', target.shape)
 
         CE = self.ce(output, target)
-        bs = target.size(0)
-        num_classes = output.size(1)
-        dims = (0, 2)
-
-        cl_output = soft_skeletonize(output)
-        output = output.log_softmax(dim=1).exp()
-        cl_output = cl_output.log_softmax(dim=1).exp()
-
-        output = output.view(bs, num_classes, -1)
-        cl_output = cl_output.view(bs, num_classes, -1)
-
-        target = target.view(bs, -1)
-        target_skeleton = target_skeleton.view(bs, -1)
-
-        target = F.one_hot(target, num_classes)  # N,H*W -> N,H*W, C
-        target = target.permute(0, 2, 1)  # H, C, H*W
-        target_skeleton = F.one_hot(target_skeleton, num_classes)  # N,H*W -> N,H*W, C
-        target_skeleton = target_skeleton.permute(0, 2, 1)  # H, C, H*W
-
-        # print('cl_ouput_shape:', cl_output.shape, 'target_skeleton:', target_skeleton.shape)
-        # print('output_shape:', output.shape, 'target_shape:', target.shape)
-
-        iflat = norm_intersection(cl_output, target.type_as(cl_output),
-                                  smooth=self.smooth, eps=self.eps, dims=dims)
-        tflat = norm_intersection(target_skeleton, output.type_as(target_skeleton),
-                                  smooth=self.smooth, eps=self.eps, dims=dims)
-
-        intersection = iflat * tflat
-
-        # print('iflat', iflat, 'tflat', tflat, 'intersection', intersection)
-
-        scores = (2.0 * intersection) / (iflat + tflat)
-        # print('scores', scores)
-        loss = 1.0 - scores
-        mask = target.sum(dims) > 0
-        loss *= mask.to(loss.dtype)
-
-        # scores = soft_dice_score(output, target.type_as(output), smooth=self.smooth, eps=self.eps, dims=dims)
-        # loss = 1.0 - scores
+        # bs = target.size(0)
+        # num_classes = output.size(1)
+        # dims = (0, 2)
         #
+        # cl_output = soft_skeletonize(output)
+        # output = output.log_softmax(dim=1).exp()
+        # cl_output = cl_output.log_softmax(dim=1).exp()
+        #
+        # output = output.view(bs, num_classes, -1)
+        # cl_output = cl_output.view(bs, num_classes, -1)
+        #
+        # target = target.view(bs, -1)
+        # target_skeleton = target_skeleton.view(bs, -1)
+        #
+        # target = F.one_hot(target, num_classes)  # N,H*W -> N,H*W, C
+        # target = target.permute(0, 2, 1)  # H, C, H*W
+        # target_skeleton = F.one_hot(target_skeleton, num_classes)  # N,H*W -> N,H*W, C
+        # target_skeleton = target_skeleton.permute(0, 2, 1)  # H, C, H*W
+        #
+        # # print('cl_ouput_shape:', cl_output.shape, 'target_skeleton:', target_skeleton.shape)
+        # # print('output_shape:', output.shape, 'target_shape:', target.shape)
+        #
+        # iflat = norm_intersection(cl_output, target.type_as(cl_output),
+        #                           smooth=self.smooth, eps=self.eps, dims=dims)
+        # tflat = norm_intersection(target_skeleton, output.type_as(target_skeleton),
+        #                           smooth=self.smooth, eps=self.eps, dims=dims)
+        #
+        # intersection = iflat * tflat
+        #
+        # # print('iflat', iflat, 'tflat', tflat, 'intersection', intersection)
+        #
+        # scores = (2.0 * intersection) / (iflat + tflat)
+        # # print('scores', scores)
+        # loss = 1.0 - scores
         # mask = target.sum(dims) > 0
         # loss *= mask.to(loss.dtype)
-        CL = loss.mean()
+        #
+        # # scores = soft_dice_score(output, target.type_as(output), smooth=self.smooth, eps=self.eps, dims=dims)
+        # # loss = 1.0 - scores
+        # #
+        # # mask = target.sum(dims) > 0
+        # # loss *= mask.to(loss.dtype)
+        # CL = loss.mean()
+        #
+        # to_loss = 0.5 * (CE + CL)
 
-        to_loss = 0.5 * (CE + CL)
-
-        return to_loss
+        return CE
